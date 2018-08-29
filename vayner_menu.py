2185436b6a66d297fe2b954b642861a385bd4906
vp-kli <kat.li@vaynermedia.com>
2018-08-29 15:35:03 -0400

    added render setup and light section

--- vayner_menu.py
+++ vayner_menu.py
@@ -4,7 +4,7 @@ import pymel.core as pm
 main_maya_window = pm.language.melGlobals['gMainWindow']
 
 # Build a menu and parent underthe Maya Window
-vayner_menu = pm.menu('Vayner', parent=main_maya_window)
+vayner_menu = pm.menu('Vayner', parent=main_maya_window, tearOff=1)
 
 # Build a menu item and parent under the 'Vayner' menu
 # set project - updates workspace and scenes directory
@@ -42,6 +42,9 @@ publish_scene_item = pm.menuItem(label="Publish Scene",
                                  command=publish_scene,
                                  parent=vayner_menu)
 
+# add divider for artist specific scripts
+pm.menuItem(divider=1, dividerLabel="Lighting")
+
 # render setup - for lighters to prep there render globals
 render_setup = """
 from shotgun import render_setup as sg
@@ -51,3 +54,18 @@ sg.get_window()
 render_setup_item = pm.menuItem(label="Render Setup",
                                 command=render_setup,
                                 parent=vayner_menu)
+
+"""
+# delete Vayner menu
+import pymel.core as pm
+main_maya_window = pm.language.melGlobals['gMainWindow']
+if pm.menu('Vayner', q=1, exists=1):
+    pm.deleteUI('Vayner')
+"""
+
+"""
+# load menu from script editor
+import pymel.core as pm
+from shotgun import vayner_menu as sg
+reload(sg)
+"""
