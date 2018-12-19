cf30b35f06f54e1d36f0b138a5506493b8b17eec
Kathy <kat.li@vaynermedia.com>
2018-12-19 12:52:58 -0500

    deleted ui class/functions

--- set_project.py
+++ set_project.py
@@ -11,10 +11,12 @@ root = r"/Users/kathyhnali/Documents/Clients/Vayner Production/04_Maya"
 
 
 def get_project_path(project_path=None):
+    # used for remote-work, change the root variable
     if root:
         project_path = root
         return project_path
 
+    # used for in-office development
     client_brand, sub_brand = [
         sg.find_one("Project", [["name", "is", project_name]], [i, "name"])[i]["name"] for i in
         ["sg_client", "sg_brand"]]
@@ -73,8 +75,6 @@ def get_window():
 class MyWindow(QtWidgets.QDialog):
     def __init__(self):
         self.ui = self.import_ui()
-        self.scene_processes, self.asset_types, self.scene_path = None, None, None
-        self.project_path = get_project_path() + "/scenes"
         self.init_ui()
         self.setup_ui()
 
@@ -88,46 +88,7 @@ class MyWindow(QtWidgets.QDialog):
         return ui
 
     def init_ui(self):
-        self.scene_processes = get_subfolders(self.project_path, parent_folder="scenes")
-        self.ui.scene_cbx.addItems(sorted(self.scene_processes))
-        self.ui.scene_cbx.currentIndexChanged.connect(self.setup_ui)
-        self.ui.set_project_btn.clicked.connect(self.run)
         return
 
     def setup_ui(self):
-        self.scene_path = self.project_path + "/" + self.ui.scene_cbx.currentText()
-        pfld = self.ui.scene_cbx.currentText().split("_", 1)[1]
-        self.asset_types = get_subfolders(self.scene_path, parent_folder=pfld)
-        self.ui.asset_cbx.clear()
-        self.ui.asset_cbx.addItems(sorted(self.asset_types))
-        return
-
-    def run(self):
-        scene_selection = self.ui.scene_cbx.currentText()
-        asset_selection = self.ui.asset_cbx.currentText()
-
-        # relative paths
-        scene_path = "scenes/{}".format(scene_selection)
-        asset_path = ""  # None doesn't work on workspace variables
-
-        # update scene path and set asset path
-        # if the scene is layouts, dynamics, lighting, or animation
-        if asset_selection:
-            scene_path += "/{}".format(asset_selection)
-
-        if scene_selection.split("_")[1] in ["Layouts", "Dynamics", "Lighting", "Animation"]:
-            cache_folder = None
-            for sp in self.scene_processes:
-                if "Cache" in sp:
-                    cache_folder = sp
-                    break
-
-            asset_path = "{}/{}/{}".format(cache_folder, scene_selection, asset_selection)
-
-        # set the project
-        set_project(project_path=get_project_path(),
-                    scene_path=scene_path,
-                    alembic_cache=asset_path)
-
-        self.ui.close()
         return
