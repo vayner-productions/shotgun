285c374df36f88949195e3980f51f45233033318
vp-kli <kat.li@vaynermedia.com>
2019-01-14 18:02:04 -0500

    imports render cam and creates top node set up if it doesn't exist

--- initialize_camera.py
+++ initialize_camera.py
@@ -47,16 +47,33 @@ class MyWindow(QtWidgets.QDialog):
         return ui
 
     def load_vayner(self):
-        # camera_path = ppath.path(__file__.rsplit("/", 1)[0] + "/render_cam_RIG")
         camera_path = ppath.path(__file__).dirname().joinpath("render_cam_RIG")
         camera_file = sorted(camera_path.files())[::-1][0]  # ensures latest version
         nodes = pm.system.importFile(camera_file, defaultNamespace=1, returnNewNodes=1)
         top_node = pm.ls(nodes, assemblies=1)[0]
         self.ui.close()
+        print ">> loaded", top_node,
         return
 
-    def load_tracked(self):
+    def load_other(self):
+        filters = "Maya Files (*.ma *.mb);;Maya ASCII (*.ma);;Maya Binary (*.mb)"
+        workspace = pm.system.Workspace()
+        shot = ppath.path(workspace.fileRules["scene"]).basename()
+        camera_path = ppath.path(workspace.getName()).joinpath("scenes", "03_Cameras", shot)
+        camera_file = pm.fileDialog2(ff=filters, dir=camera_path, fm=1)[0]
+
+        nodes = pm.system.importFile(camera_file, defaultNamespace=1, returnNewNodes=1)
+        if "render_cam_RIG" not in nodes:
+            top_nodes = pm.ls(nodes, assemblies=1)
+            camera_top_node = pm.group(em=1, name="render_cam_RIG")
+            camera_top_node.setAttr("v", lock=1, k=0, cb=0)
+            for at in "trs":
+                for ax in "xyz":
+                    camera_top_node.setAttr(at + ax, lock=1, k=0, cb=0)
+
+            pm.select(top_nodes, camera_top_node)  # for testing, delete later
         self.ui.close()
+        print ">> loaded camera"
         return
 
     def publish_camera(self):
@@ -64,9 +81,8 @@ class MyWindow(QtWidgets.QDialog):
         return
 
     def init_ui(self):
-        self.load_vayner()
         self.ui.vayner_btn.clicked.connect(self.load_vayner)
-        self.ui.other_btn.clicked.connect(self.load_tracked)
+        self.ui.other_btn.clicked.connect(self.load_other)
         self.ui.publish_btn.clicked.connect(self.publish_camera)
         return
 
