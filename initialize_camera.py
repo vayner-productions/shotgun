eddcafb86232130a2175465231806e080e7b8aaa
vp-kli <kat.li@vaynermedia.com>
2019-01-15 14:40:57 -0500

    exporting top node as reference

--- initialize_camera.py
+++ initialize_camera.py
@@ -46,22 +46,28 @@ class MyWindow(QtWidgets.QDialog):
         ui_file.close()
         return ui
 
-    def load_vayner(self):
-        camera_path = ppath.path(__file__).dirname().joinpath("render_cam_RIG")
-        camera_file = sorted(camera_path.files())[::-1][0]  # ensures latest version
+    def load_vayner(self, camera_file=None):
+        if camera_file is None:
+            camera_path = ppath.path(__file__).dirname().joinpath("render_cam_RIG")
+            camera_file = sorted(camera_path.files())[::-1][0]  # ensures latest version
         nodes = pm.system.importFile(camera_file, defaultNamespace=1, returnNewNodes=1)
         top_node = pm.ls(nodes, assemblies=1)[0]
         self.ui.close()
-        print ">> loaded", top_node,
-        return
+        print ">> loaded: {}".format(top_node),
+        return top_node
 
-    def load_other(self):
-        filters = "Maya Files (*.ma *.mb);;Maya ASCII (*.ma);;Maya Binary (*.mb)"
-        workspace = pm.system.Workspace()
-        shot = ppath.path(workspace.fileRules["scene"]).basename()
-        camera_path = ppath.path(workspace.getName()).joinpath("scenes", "03_Cameras", shot)
-        camera_file = pm.fileDialog2(ff=filters, dir=camera_path, fm=1)[0]
+    def load_other(self, camera_file=None):
+        if camera_file is None:
+            # open file dialog to 03_Cameras and filter for maya files
+            filters = "Maya Files (*.ma *.mb);;Maya ASCII (*.ma);;Maya Binary (*.mb)"
+            workspace = pm.system.Workspace()
+            shot = ppath.path(workspace.fileRules["scene"]).basename()
+            camera_path = ppath.path(workspace.getName()).joinpath("scenes", "03_Cameras", shot).normpath()
+            camera_file = pm.fileDialog2(ff=filters, dir=camera_path, fm=1)[0]
 
+        # import camera and place in import group to easily see
+        # create render_cam_RIG null for user to place camera in
+        # select import and null for user to see results
         nodes = pm.system.importFile(camera_file, defaultNamespace=1, returnNewNodes=1)
         if "render_cam_RIG" not in nodes:
             top_nodes = pm.ls(nodes, assemblies=1)
@@ -70,13 +76,47 @@ class MyWindow(QtWidgets.QDialog):
             for at in "trs":
                 for ax in "xyz":
                     camera_top_node.setAttr(at + ax, lock=1, k=0, cb=0)
-
-            pm.select(top_nodes, camera_top_node)  # for testing, delete later
+            camera_filename = ppath.path(camera_file).basename().stripext()
+            imported = pm.group(top_nodes, name=camera_filename+"_IMPORT")
+            pm.select(imported, camera_top_node)  # for testing, delete later
         self.ui.close()
-        print ">> loaded camera"
+        print ">> loaded: {}".format(imported),
         return
 
     def publish_camera(self):
+        camera_top_node = pm.ls("render_cam_RIG")[0]
+        cameras = camera_top_node.getChildren(ad=1, typ="camera")
+        if len(cameras) > 1:
+            pm.warning(">> too many cameras in render_cam_RIG, choose one")
+            return
+        elif len(cameras) == 0:
+            pm.warning("place camera in render_cam_RIG")
+            return
+        else:
+            render_cam = cameras[0].getParent().rename("render_cam")
+            workspace = pm.system.Workspace()
+            shot = ppath.path(workspace.fileRules["scene"]).basename()
+            camera_path = ppath.path(workspace.getName()).joinpath("published", "03_Cameras", shot).normpath()
+            camera_path.makedirs_p()
+
+            pm.select(camera_top_node)
+            camera_files = sorted(camera_path.files("{}_original.*.ma".format(shot)))[::-1]
+            if camera_files:
+                latest_file = camera_files[0].split(".")
+                latest_file[1] = str(int(latest_file[1]) + 1).zfill(4)
+                camera_file = ppath.path(".".join(latest_file))
+                pm.system.exportAsReference(camera_file, namespace=":")
+            else:
+                camera_file = camera_path.__div__("{}_original.0001.ma".format(shot))
+                pm.system.exportAsReference(camera_file, namespace=":")
+            print ">> published render_cam_RIG to shotgun",
+
+        try:
+            imported = pm.ls("*_IMPORT")[0]
+            if not imported.getChildren():
+                pm.delete(imported)
+        except:
+            pass
         self.ui.close()
         return
 
@@ -85,9 +125,3 @@ class MyWindow(QtWidgets.QDialog):
         self.ui.other_btn.clicked.connect(self.load_other)
         self.ui.publish_btn.clicked.connect(self.publish_camera)
         return
-
-"""
-import pymel.util.common as common
-root = r"A:\Animation\Shotgun\System\Tools\shotgun\render_cam_RIG"
-vayner_camera = sorted(common.path(root).files("render_cam.*.ma"))[::-1][0]
-"""
