00efe4c57db5cdc3f939dab38627391c654deeb8
vp-kli <kat.li@vaynermedia.com>
2019-01-28 15:37:56 -0500

    using __init__.py

--- update_timeline.py
+++ update_timeline.py
@@ -1,22 +1,23 @@
-from shotgun import checkout_scene
-import pymel.core as pm
-import sgtk.platform
-engine = sgtk.platform.current_engine()
+from . import sg, project
+from pymel.core.system import Workspace, warning
+import pymel.util.path as path
 
 
-def update_timeline():
-    if "Shot" in pm.workspace.fileRules["scene"]:
-        sg = engine.shotgun
-        project = sg.find_one("Project", [["name", "is", engine.context.project["name"]]])
-        entity = checkout_scene.get_entity(pm.workspace.fileRules["scene"])
-        frame_range = sg.find_one("Shot",
-                                  [["project", "is", project], ["id", "is", entity["id"]]],
-                                  ["sg_frame_range"]
-                                  )["sg_frame_range"]
+def update_timeline(frame_range=None):
+    try:
+        workspace = Workspace()
+        shot_code = path(workspace.fileRules["scene"]).basename()  # Shot_###
+
+        filter = [
+            ["project", "is", project],
+            ["code", "is", shot_code]
+        ]
+        frame_range = sg.find_one("Shot", filter, ["sg_frame_range"])["sg_frame_range"]
 
         start, end = [int(t) for t in frame_range.split("-")]
         pm.playbackOptions(min=start, max=end)
-        print ">> updated timeline: {}".format(frame_range),
-    else:
-        pm.warning(">> for Shots only")
+    except:
+        warning("Update timeline failed, set project to shot.")
+        pass
+    print ">> updated timeline: {}".format(frame_range),
     return
