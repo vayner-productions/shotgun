d0ab49c09d80eab41aa56e6f933f876f1ce94599
vp-kli <kat.li@vaynermedia.com>
2019-01-29 18:18:56 -0500

    commented, can't initialize current sg engine

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@
 Common variables used in all shotgun tools.
 """
 
-from sgtk import platform
-
-root = None
-engine = platform.current_engine()
-sg = engine.shotgun
-project = sg.find_one("Project", [["name", "is", engine.context.project["name"]]])
+# from sgtk import platform
+#
+# root = None
+# engine = platform.current_engine()
+# sg = engine.shotgun
+# project = sg.find_one("Project", [["name", "is", engine.context.project["name"]]])
