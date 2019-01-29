03446b92cdeaf136d6b78c542b76613d92e76f6b
vp-kli <kat.li@vaynermedia.com>
2019-01-29 18:20:01 -0500

    reverted to before first pass of class Checkout was completed

--- vayner_menu.py
+++ vayner_menu.py
@@ -19,46 +19,31 @@ set_project_item = pm.menuItem(label="Set Project",
                                parent=vayner_menu)
 
 
-# check out latest working version given project settings
-checkout_working_version = """
+# check out scene - open the latest published scene
+checkout_scene = """
 import shotgun.checkout_scene as sg
 
 reload(sg)
-checkout = sg.Checkout()
-checkout.run(checkout_type="processed")
+sg.checkout_scene()
 """
-checkout_working_version_item = pm.menuItem(label="Checkout Working Version",
-                                  command=checkout_working_version,
+checkout_scene_item = pm.menuItem(label="Checkout Scene",
+                                  command=checkout_scene,
                                   parent=vayner_menu)
 
 
-# checkout published scene as working version with naming convention based on set project
-checkout_published = """
-import shotgun.checkout_scene as sg
-
-reload(sg)
-checkout = sg.Checkout()
-checkout.run(checkout_type="published")
-"""
-checkout_published_item = pm.menuItem(label="Checkout Published Version",
-                                  command=checkout_published,
-                                  parent=vayner_menu)
-
-
-# increment and save with naming convention based on set project
+# check out scene - open the latest published scene
 increment_and_save = """
-import shotgun.checkout_scene as sg
+from shotgun import checkout_scene as sg
 
 reload(sg)
-checkout = sg.Checkout()
-checkout.run(checkout_type="increment")
+sg.increment_and_save(None)
 """
 increment_and_save_item = pm.menuItem(label="Increment and Save",
                                   command=increment_and_save,
                                   parent=vayner_menu)
 
 
-# build scene references all available assets assigned to shot
+# check out scene - open the latest published scene
 build_scene = """
 from shotgun import build_scene as sg
 
@@ -66,11 +51,11 @@ reload(sg)
 sg.get_window()
 """
 build_scene_item = pm.menuItem(label="Build Scene",
-                               command=build_scene,
-                               parent=vayner_menu)
+                                  command=build_scene,
+                                  parent=vayner_menu)
 
 
-# update timeline based on shotgun site's frame range
+# update timeline - timeline reflects sg_frame_range
 update_timeline = """
 import shotgun.update_timeline as sg
 
@@ -78,10 +63,10 @@ reload(sg)
 sg.update_timeline()
 """
 update_timeline_item = pm.menuItem(label="Update Timeline",
-                                   command=update_timeline,
-                                   parent=vayner_menu)
+                                 command=update_timeline,
+                                 parent=vayner_menu)
 
-# organize textures -- not sure if anyone's using it
+# publish scene - increment and save this file and update shotgun fields
 organize_textures = """
 import shotgun.organize_textures as sg
 
@@ -89,8 +74,8 @@ reload(sg)
 sg.organize_textures()
 """
 organize_textures_item = pm.menuItem(label="Organize Textures",
-                                     command=organize_textures,
-                                     parent=vayner_menu)
+                                 command=organize_textures,
+                                 parent=vayner_menu)
 
 # publish scene - increment and save this file and update shotgun fields
 publish_scene = """
@@ -115,3 +100,16 @@ sg.get_window()
 render_setup_item = pm.menuItem(label="Render Setup",
                                 command=render_setup,
                                 parent=vayner_menu)
+
+"""
+# delete Vayner menu
+import pymel.core as pm
+main_maya_window = pm.language.melGlobals['gMainWindow']
+if pm.menu('Vayner', q=1, exists=1):
+    pm.deleteUI('Vayner')
+
+# load menu from script editor
+import pymel.core as pm
+from shotgun import vayner_menu as sg
+reload(sg)
+"""
