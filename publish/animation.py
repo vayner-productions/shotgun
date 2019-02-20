9a29c48a43199090c06d587b22337b2dae9b66bb
vp-kli <kat.li@vaynermedia.com>
2019-02-20 17:54:55 -0500

    prepared two files

---
+++ publish/animation.py
@@ -0,0 +1,11 @@
+"""
+creates alembics for each referenced node
+referenced nodes include:
+- camera (skip alembic)
+- char (anim)
+- set (no anim)
+- props (anim)
+publishes camera..option?
+option to skip playblast
+versioning changes in SG site..
+"""
