0b4c7ca6cc4386ad09b5221e15aa91f37dd45355
vp-kli <kat.li@vaynermedia.com>
2019-02-21 14:33:30 -0500

    shotgun tools loads after shotgun api loads; can recycle sg enging and project

--- update_timeline.py
+++ update_timeline.py
@@ -1,5 +1,6 @@
 from . import sg, project
 from pymel.core.system import Workspace, warning
+from pymel.core.animation import playbackOptions
 import pymel.util.path as path
 
 
@@ -15,7 +16,7 @@ def update_timeline(frame_range=None):
         frame_range = sg.find_one("Shot", filter, ["sg_frame_range"])["sg_frame_range"]
 
         start, end = [int(t) for t in frame_range.split("-")]
-        pm.playbackOptions(min=start, max=end)
+        playbackOptions(min=start, max=end)
     except:
         warning("Update timeline failed, set project to shot.")
         pass
