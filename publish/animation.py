17d0c6e0b7e8f60733be8a2ffd1e884ac8b4f8f2
vp-kli <kat.li@vaynermedia.com>
2019-02-25 13:11:05 -0500

    added base template

--- publish/animation.py
+++ publish/animation.py
@@ -9,5 +9,36 @@ publishes camera..option?
 option to skip playblast
 versioning changes in SG site..
 """
-print ">>", MY_CONSTANT
 from . import *
+from PySide2 import QtCore, QtWidgets, QtUiTools
+
+
+def get_window():
+    global mw
+    try:
+        mw.ui.close()
+    except:
+        pass
+
+    mw = MyWindow()
+    mw.ui.show()
+
+
+class Publish:
+    def __init__(self):
+        return
+
+
+class MyWindow(QtWidgets.QDialog):
+    def __init__(self):
+        self.ui = self.import_ui()
+        return
+
+    def import_ui(self):
+        ui_path = __file__.split(".")[0] + ".ui"
+        loader = QtUiTools.QUiLoader()
+        ui_file = QtCore.QFile(ui_path)
+        ui_file.open(QtCore.QFile.ReadOnly)
+        ui = loader.load(ui_file)
+        ui_file.close()
+        return ui
