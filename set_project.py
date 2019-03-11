f397beae8061db12c9aa56afd4bbd1263bef7d41
vp-kli <kat.li@vaynermedia.com>
2019-03-11 18:10:25 -0400

    SetProject __init__()

--- set_project.py
+++ set_project.py
@@ -3,6 +3,9 @@ TODO: IN ANIMATION, UPDATE "alembicCache" FILE RULLE cache/alembic --> scenes/06
 """
 from . import *
 from PySide2 import QtCore, QtWidgets, QtUiTools
+from pymel.util import path
+from pymel.core.system import Workspace, warning
+workspace = Workspace()
 
 
 def get_window():
@@ -17,7 +20,64 @@ def get_window():
 
 
 class SetProject:
-    def __init__(self, scenes=None):
+    def __init__(self, process=None, entity=None):
+        # --- process / entity_type ---
+        template = [
+            "00_Tests",
+            "01_Assets",
+            "02_Rigs",
+            "03_Cameras",
+            "04_Layouts",
+            "05_Dynamics",
+            "06_Cache",
+            "07_Lighting",
+            "08_Animation"
+        ]
+
+        for dir in template:
+            if process.lower() in dir.lower():
+                self.process = dir
+                break
+
+        if self.process is "01_Assets" or self.process is "02_Rigs":
+            self.entity_type = "Asset"
+        elif self.process is "00_Tests":
+            pass
+        else:
+            self.entity_type = "Shot"
+
+        # --- entity ---
+        self.entity = sg.find_one(
+            self.entity_type,  # Asset / Shot
+            [["project", "is", project], ["code", "ends_with", entity.lower()]],
+            ["code"]
+        )
+
+        # --- project_path ---
+        # used for remote-work, change the root variable
+        if root:
+            self.project_path = path(root).normpath()
+            return
+
+        # used for in-office development
+        data = sg.find_one(
+            "Project",
+            [
+                ["id", "is", project["id"]]
+            ],
+            [
+                "sg_client",
+                "sg_brand",
+                "name"
+            ]
+        )
+        client_brand = data["sg_client"]["name"]
+        sub_brand = data["sg_brand"]["name"]
+        project_name = data["name"]
+        project_path = r"{}/Animation/Projects/Client/{}/{}/{}/Project Directory/02_Production/04_Maya".format(
+            media_space, client_brand, sub_brand, project_name)
+
+        self.project_path = path(project_path).normpath()
         return
 
 
