a69887762a98dd253e1ada527ae382f5917d66f2
vp-kli <kat.li@vaynermedia.com>
2019-03-12 18:10:23 -0400

    record_json() - allows user preferences for set_project() to be recorded

--- set_project.py
+++ set_project.py
@@ -19,10 +19,18 @@ def get_window():
     mw.ui.show()
 
 
-class SetProject:
-    def __init__(self, process=None, entity=None):
-        # --- process / entity_type ---
-        template = [
+class SetProject(object):
+    def __init__(self, **kwargs):
+        allowed_keys = {
+            "process",  # scene process - 08_Animation
+            "entity_type",  # for sg.find() - Asset/Shot
+            "entity",  # to find name is Assets/Shots page - 001_Hero/Shot_001
+            "project_path"  # for setting project window - ../04_Maya
+        }
+        self.__dict__.update((k, None) for k in allowed_keys)
+        self.__dict__.update((k, v) for k, v in kwargs.items())
+
+        self.template = {
             "00_Tests",
             "01_Assets",
             "02_Rigs",
@@ -32,10 +40,12 @@ class SetProject:
             "06_Cache",
             "07_Lighting",
             "08_Animation"
-        ]
+        }
 
-        for dir in template:
-            if process.lower() in dir.lower():
+    def process_data(self):
+        # --- process / entity_type ---
+        for dir in self.template:
+            if self.process.lower() in dir.lower():
                 self.process = dir
                 break
 
@@ -49,7 +59,7 @@ class SetProject:
         # --- entity ---
         self.entity = sg.find_one(
             self.entity_type,  # Asset / Shot
-            [["project", "is", project], ["code", "ends_with", entity.lower()]],
+            [["project", "is", project], ["code", "ends_with", self.entity.lower()]],
             ["code"]
         )
 
@@ -80,9 +90,105 @@ class SetProject:
         self.project_path = path(project_path).normpath()
         return
 
+    def record_json(self, process="00_Tests", entity_name="incrementalSave", data=None, update=False):
+        """
+        pymel.core.language.OptionVarDict records the workstation user's preferences
 
-class MyWindow(QtWidgets.QDialog):
-    def __init__(self):
+        This method records set project preferences of the user logged into sg desktop.
+        :param process:
+        :param entity_name:
+        :param data: json object data
+        :param update: edits json file if changes were made, ignores param defaults
+        :return process, entity_name:
+        """
+        import json
+        from sgtk.authentication import ShotgunAuthenticator
+
+        auth = ShotgunAuthenticator()
+        user = str(auth.get_user())
+
+        script_dir = path(workspace.expandName(workspace.fileRules["scripts"]))
+        set_project_file = script_dir.joinpath("set_project.json").normpath()
+
+        # create json file and use param defaults
+        if not set_project_file.exists():
+            data = {
+                user: {
+                    "process": process,
+                    "entity": entity_name
+                }
+            }
+            with set_project_file.open(mode="a") as new_file:
+                json.dump(data, new_file, indent=4, separators=(',', ': '))
+            return process, entity_name
+
+        try:
+            # determine whether preferences need to be updated for user
+            with set_project_file.open(mode="r") as read_file:
+                data = json.load(read_file)
+
+            param_default = process is "00_Tests" or entity_name is "incrementalSave"
+            process_changed = process is not data[user]["process"]
+            entity_name_changed = entity_name is not data[user]["entity"]
+
+            if not param_default and process_changed or entity_name_changed:
+                update = True
+            else:
+                return process, entity_name
+        except KeyError:
+            # add new user to json file
+            data = {
+                user: {
+                    "process": process,
+                    "entity": entity_name
+                }
+            }
+            with set_project_file.open(mode="a") as append_file:
+                json.dump(data, append_file, indent=4, separators=(',', ': '))
+            return process, entity_name
+
+        # update preferences for existing users json file
+        if update:
+            data[user]["process"] = process
+            data[user]["entity"] = entity_name
+
+            with set_project_file.open(mode="w") as edit_file:
+                json.dump(data, edit_file, indent=4, separators=(',', ': '))
+            return process, entity_name
+
+    def run(self):
+        """
+        updates the project window - current project is 04_Maya and location is project path
+
+        template changes are also made to scene, shaders, and Alembic file rules -
+        scenes/scene process folder/entity
+            - scenes/08_Animation/Shot_001
+            - scenes/01_Assets/001_hero
+        data/001_Shaders
+        scenes/06_Cache/08_Animation/Shot_###
+
+        directories are made in create_project_directory.py OR it comes from Vayner project template
+        :return:
+        """
+        # create_project_directory.py
+        workspace.open(self.project_path)
+
+        workspace.fileRules["scene"] = "/".join(["scenes", self.process, self.entity["code"]])
+
+        if self.entity_type is "Shot":
+            workspace.fileRules["Alembic"] = "/".join(["scenes", "06_Cache", "08_Animation", self.entity["code"]])
+
+        # Vayner project template
+        workspace.fileRules["shaders"] = "/".join(["data", "001_Shaders"])
+
+        # Save preferences to script/set_project.json
+        self.record_json(process=self.process, entity_name=self.entity["code"])
+        return
+
+
+class MyWindow(SetProject, QtWidgets.QDialog):
+    def __init__(self, **kwargs):
+        super(MyWindow, self).__init__(**kwargs)
         self.ui = self.import_ui()
         self.init_ui()
 
@@ -95,8 +201,19 @@ class MyWindow(QtWidgets.QDialog):
         ui_file.close()
         return ui
 
+    def change_asset_items(self):
+        print ">> changed asset"
+        return
+
     def init_ui(self):
-        self.ui.asset_cbx
-        self.ui.scene_cbx
-        self.ui.set_project_btn
+        exclude = {
+            "04_Layouts",
+            "06_Cache"
+        }
+        items = sorted(self.template.difference(exclude))
+        self.ui.scene_cbx.addItems(items)
+        self.ui.scene_cbx.currentTextChanged.connect(self.change_asset_items)
+        # self.ui.scene_cbx.setCurrentText()
+        # self.ui.asset_cbx
+        # self.ui.set_project_btn
         return
