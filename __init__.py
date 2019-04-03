1cf1202389a1b13f7c5fe06e7721abc956c5c126
vp-kli <kat.li@vaynermedia.com>
2019-04-03 13:21:47 -0400

    more variables on init

--- __init__.py
+++ __init__.py
@@ -11,12 +11,13 @@ project = sg.find_one(
 media_space = sg.find_one(
     "CustomNonProjectEntity28",
     filters=[["sg_projects", "is", project]],
-    fields=["sg_mapped_letter"]
-)["sg_mapped_letter"]
-
+    fields=["sg_mapped_letter", "sg_unc_path"]
+)
+mapped_letter = media_space["sg_mapped_letter"]
+unc_path = media_space["sg_unc_path"]
 
 root = None  # the drive you're working on i.e. Mac/../04_Maya
 
 
-__all__ = ["root", "sg", "project", "media_space"]
+__all__ = ["root", "sg", "project", "mapped_letter", "unc_path"]
 
