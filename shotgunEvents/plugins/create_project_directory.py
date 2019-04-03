106354f02304b1947ca442f473aac5b08c782196
vp-kli <kat.li@vaynermedia.com>
2019-04-03 16:26:14 -0400

    create.project()

--- shotgunEvents/plugins/create_project_directory.py
+++ shotgunEvents/plugins/create_project_directory.py
@@ -1,4 +1,5 @@
-from pymel.util import path
+from os import path, mkdir
+import shutil
 import shotgun_api3
 
 
@@ -14,7 +15,7 @@ def registerCallbacks(reg):
         "Shotgun_Shot_Change": ["code"]
     }
     server = "https://vaynerproductions.shotgunstudio.com"
-    script_name = path(__file__).basename()
+    script_name = path.basename(__file__).split(".")[0] + ".py"  # no pyc files
     script_key = file_object.readline()
 
     file_object.close()
@@ -32,11 +33,74 @@ def registerCallbacks(reg):
 
 
 def create_project_directory(sg, logger, event, args):
+    create = CreateDirectory(sg, logger, event)
     if "Shotgun_Project_Change" == event["event_type"]:
-        logger.info(">> PROJECT CHANGED")
+        create.project()
     elif "Shotgun_Shot_Change" == event["event_type"]:
-        logger.info(">> SHOT CHANGED")
+        create.shot()
     elif "Shotgun_Asset_Change" == event["event_type"]:
-        logger.info(">> ASSET CHANGED")
-    logger.info(event)
+        create.asset()
+    # logger.info(event)
     return
+
+
+class CreateDirectory(object):
+    def __init__(self, sg, logger, event):
+        self.sg = sg
+        self.logger = logger
+        self.event = event
+
+        # project path is used in all methods
+        project = event["entity"]
+
+        unc_path = "sg_media_space.CustomNonProjectEntity28.sg_unc_path"
+        client = "sg_client.CustomNonProjectEntity15.code"
+        brand = "sg_brand.CustomNonProjectEntity29.code"
+        project_name = "name"
+
+        fields = sg.find_one(
+            "Project",
+            filters=[["id", "is", project["id"]]],
+            fields=[client, brand, unc_path, project_name]
+        )
+
+        self.project_directory = path.normpath(path.join(
+            fields[unc_path],
+            "Animation/Projects/Client",
+            fields[client],
+            fields[brand],
+            fields[project_name]
+        ))
+        return
+
+    def project(self):
+        """
+        creates project directory from template for production and another project directory on A: for tracking
+        :return:
+        """
+        self.logger.info(">> PROJECT CHANGED")
+
+        template = path.normpath(
+            "//genesisnx/genesisnx/Animation/Directory Source/New Project/0000_Project"
+        )
+        a_drive = path.normpath(path.join(
+            "//genesisnx/genesisnx",
+            self.project_directory[self.project_directory.index("Animation"):]
+        ))
+
+        try:
+            mkdir(a_drive, self.project_directory)
+            shutil.copytree(template, self.project_directory)
+        except:
+            pass
+        return
+
+    def shot(self):
+        self.logger.info(">> SHOT CHANGED")
+        return
+
+    def asset(self):
+        # create asset folder
+        # create source images folder
+        self.logger.info(">> ASSET CHANGED")
+        return
