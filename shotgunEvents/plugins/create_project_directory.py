e184e1e801ce48fac81115628c8636785e829bfb
vp-kli <kat.li@vaynermedia.com>
2019-04-08 15:18:37 -0400

    CreateEntity()

--- shotgunEvents/plugins/create_project_directory.py
+++ shotgunEvents/plugins/create_project_directory.py
@@ -172,3 +172,39 @@ class CreateDirectory(object):
             self.logger.info(">> {}".format(directory))
         return
 
+
+class CreateEntity(object):
+    def __init__(self, sg, logger, event):
+        self.sg = sg
+        self.logger = logger
+        self.event = event
+
+        # project = self.event["project"]
+        # shot = self.event["entity"]
+        self.logger.info(">>>>> {} {}".format(self.event["entity"]), self.event["project"])
+        return
+
+    def camera(self):
+        self.logger.info(">> CREATING CAMERA ENTITY")
+
+        # camera_entity = self.sg.find_one(
+        #     "Shot",
+        #     [["project", "is", self.project],
+        #      ["code", "is", new_shot]],
+        #     ["sg_camera"]
+        # )["sg_camera"]
+
+        # if not camera_entity:
+        #     shot_entity = event["entity"]
+        #     data = {
+        #         "project": project,
+        #         "code": shot_entity["name"] + "_Cam",
+        #         "shot_sg_camera_shots": [shot_entity],
+        #     }
+        #     sg.create("Camera", data)
+        #     logger.info(">> created and link camera entity to shot")
+        return
+
+    def animation(self):
+        self.logger.info(">> CREATING CAMERA ENTITY")
+        return
