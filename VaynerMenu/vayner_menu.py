92cf2eff2336cc23354fd3b5c59a37f01edab27c
vp-kli <kat.li@vaynermedia.com>
2019-04-10 15:39:33 -0400

    reload_menu() updates menu to items removed/added

--- VaynerMenu/vayner_menu.py
+++ VaynerMenu/vayner_menu.py
@@ -5,12 +5,12 @@ import shotgun.set_project as sg; reload(sg)
 sg.get_window()
 """
 
-checkout_menu = """
+checkout_preferences = """
 from shotgun.VaynerMenu import qt_checkout_box as sg; reload(sg)
 sg.get_window()
 """
 
-checkout_saved_version = """
+checkout = """
 from shotgun.VaynerMenu import qt_checkout_box as sg; reload(sg)
 sg.checkout_saved_option()
 """
@@ -79,7 +79,10 @@ sg.get_window()
 """
 
 reload_menu = """
-print ">>>"
+from shotgun.VaynerMenu import vayner_menu as sg
+reload(sg)
+vm = sg.VaynerMenu()
+vm.run()
 """
 
 
@@ -88,150 +91,160 @@ class VaynerMenu:
         self.vayner_menu = None
         try:
             self.vayner_menu = Menu("Vayner")
+            for item in self.vayner_menu.getItemArray()[::-1]:
+                item.delete()
         except ValueError:
             self.vayner_menu = Menu("Vayner", parent="MayaWindow", tearOff=1)
+
+        reload_menu_item = MenuItem(
+            label="Reload Vayner Menu",
+            command=reload_menu,
+            parent="Vayner"
+        )
         return
 
-    def run(self):
-        self.vayner_menu.deleteAllItems()
-
-        # updates project directory
-        set_project_item = MenuItem(
-            label="Set Project",
-            command=set_project,
-            parent="Vayner"
-        )
-
-        # checkout either the published or working file
-        checkout_item = MenuItem(
-            "checkout_item",
-            label="Checkout",
-            command=checkout_saved_version,
-            parent="Vayner"
-        )
-        # creates option box for the checkout menu item
-        checkout_box_item = MenuItem(
-            optionBox=True,
-            command=checkout_menu,
-            parent="Vayner"
-        )
-
-        # increment and save current working file
-        increment_and_save_item = MenuItem(
-            label="Increment And Save",
-            command=increment_and_save,
-            parent="Vayner"
-        )
-
-        # references all available assets designated to shot
-        build_scene_item = MenuItem(
-            label="Build Scene",
-            command=build_scene,
-            parent="Vayner"
-        )
-
-        # MODELING SUBMENU
-        modeling_item = MenuItem(
-            label="Modeling",
-            subMenu=True,
-            parent="Vayner"
-        )
-
-        # publishes modeling scene
-        publish_modeling = MenuItem(
-            label="Publish Model",
-            command=publish_scene
-        )
-
-        # RIGGING SUBMENU
-        rigging_item = MenuItem(
-            label="Rigging",
-            subMenu=True,
-            parent="Vayner"
-        )
-
-        # publishes rigging scene
-        import_camera_item = MenuItem(
-            label="Publish Rig",
-            command=publish_scene
-        )
-
-        # CAMERA SUBMENU
-        camera_item = MenuItem(
-            label="Camera",
-            subMenu=True,
-            parent="Vayner"
-        )
-
-        # imports camera
-        import_camera_item = MenuItem(
-            label="Import Camera",
-            command=import_camera
-        )
-
-        # publishes camera
-        publish_camera_item = MenuItem(
-            label="Publish Camera",
-            command=publish_camera,
-        )
-
-        # ANIMATION SUBMENU
-        animation_item = MenuItem(
-            label="Animation",
-            subMenu=True,
-            parent="Vayner"
-        )
-
-        # brings up manage proxy window
-        update_timeline_item = MenuItem(
-            label="Manage Proxies",
-            command=manage_proxies,
-        )
-
-
-        # matches start/end playback to frame range on shotgun site
-        update_timeline_item = MenuItem(
-            label="Update Timeline",
-            command=update_timeline,
-        )
-
-        # publishes animation
-        update_timeline_item = MenuItem(
-            label="Publish Animation",
-            command=publish_animation,
-        )
-
-        # LIGHTING SUBMENU
-        lighting_item = MenuItem(
-            label="Lighting",
-            subMenu=True,
-            parent="Vayner"
-        )
-
-        # places textures in /sourceimages under /Assets and /HDRI
+    def add_lighting_items(self):
         organize_textures_item = MenuItem(
             label="Organize Textures",
             command=organize_textures,
         )
 
-        # templates lighters' files for render farm
         render_setup_item = MenuItem(
             label="Render Setup",
             command=render_setup,
         )
 
-        # publishes lighting
         publish_lighting_item = MenuItem(
             label="Publish Lighting",
             command=publish_scene,
         )
+        return
 
-        # # reloads menu
-        # reload_menu_item = MenuItem(
-        #     label="Reload Vayner Menu",
-        #     command=reload_menu,
-        #     parent="Vayner"
-        # )
+    def add_animation_items(self):
+        manage_proxies_item = MenuItem(
+            label="Manage Proxies",
+            command=manage_proxies,
+        )
+
+        update_timeline_item = MenuItem(
+            label="Update Timeline",
+            command=update_timeline,
+        )
+
+        publish_animation_item = MenuItem(
+            label="Publish Animation",
+            command=publish_animation,
+        )
+        return
+
+    def add_camera_items(self):
+        import_camera_item = MenuItem(
+            label="Import Camera",
+            command=import_camera
+        )
+
+        publish_camera_item = MenuItem(
+            label="Publish Camera",
+            command=publish_camera,
+        )
+        return
+
+    def add_rigging_items(self):
+        publish_rig_item = MenuItem(
+            label="Publish Rig",
+            command=publish_scene
+        )
+        return
+
+    def add_modeling_items(self):
+        publish_modeling_item = MenuItem(
+            label="Publish Model",
+            command=publish_scene
+        )
+        return
+
+    def run(self):
+        lighting_menu = MenuItem(
+            label="Lighting",
+            subMenu=True,
+            parent="Vayner",
+            insertAfter=""
+        )
+
+        self.add_lighting_items()
+
+        animation_item = MenuItem(
+            label="Animation",
+            subMenu=True,
+            parent="Vayner",
+            insertAfter=""
+        )
+
+        self.add_animation_items()
+
+        camera_item = MenuItem(
+            label="Camera",
+            subMenu=True,
+            parent="Vayner",
+            insertAfter=""
+        )
+
+        self.add_camera_items()
+
+        rigging_item = MenuItem(
+            label="Rigging",
+            subMenu=True,
+            parent="Vayner",
+            insertAfter=""
+        )
+
+        self.add_rigging_items()
+
+        modeling_item = MenuItem(
+            label="Modeling",
+            subMenu=True,
+            parent="Vayner",
+            insertAfter=""
+        )
+
+        self.add_modeling_items()
+
+        build_scene_item = MenuItem(
+            label="Build Scene",
+            command=build_scene,
+            parent="Vayner",
+            insertAfter=""
+        )
+
+        increment_and_save_item = MenuItem(
+            label="Increment And Save",
+            command=increment_and_save,
+            parent="Vayner",
+            insertAfter=""
+        )
+
+        checkout_item = MenuItem(
+            "checkout_item",
+            label="Checkout",
+            command=checkout,
+            parent="Vayner",
+            insertAfter=""
+        )
+
+        checkout_box_item = MenuItem(
+            optionBox=1,
+            label="Checkout Option",
+            command=checkout_preferences,
+            parent="Vayner",
+            insertAfter=checkout_item
+        )
+
+        set_project_item = MenuItem(
+            label="Set Project",
+            command=set_project,
+            parent="Vayner",
+            insertAfter=""
+        )
 
         # removes pyc files
         from pymel.util.common import path
@@ -239,6 +252,5 @@ class VaynerMenu:
 
         for pyc in shotgun.files("*.pyc"):
             pyc.remove_p()
-
-        print ">> Vayner Menu reloaded",
+        print ">> Vayner Menu reloaded\n",
         return
