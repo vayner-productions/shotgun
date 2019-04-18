d56b9d8a69f7f1e5bc9fbc43102415314404a687
vp-kli <kat.li@vaynermedia.com>
2019-04-18 14:49:21 -0400

    reversed

--- __init__.py
+++ __init__.py
@@ -4,19 +4,20 @@ Common variables used in all shotgun tools.
 from sgtk import platform
 engine = platform.current_engine()
 sg = engine.shotgun
-
-sg_mapped letter = "sg_media_space.CustomNonProjectEntity28.sg_mapped_letter"
-sg_unc_path = "sg_media_space.CustomNonProjectEntity28.sg_unc_path"
-
 project = sg.find_one(
     "Project",
-    fields=[["name", "is", engine.context.project["name"]]],
-    filters=[sg_mapped_letter, sg_unc_path]
+    [["name", "is", engine.context.project["name"]]]
 )
-
-mapped_letter = project[sg_mapped_letter]
-unc_path = project[sg_unc_path]
+media_space = sg.find_one(
+    "CustomNonProjectEntity28",
+    filters=[["sg_projects", "is", project]],
+    fields=["sg_mapped_letter", "sg_unc_path"]
+)
+mapped_letter = media_space["sg_mapped_letter"]
+unc_path = media_space["sg_unc_path"]
 
 root = None  # the drive you're working on i.e. Mac/../04_Maya
 
+
 __all__ = ["root", "sg", "project", "mapped_letter", "unc_path"]
+
