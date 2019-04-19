9f7f860c7fd6f38320a89d373bfb1ab6feb93bbc
vp-kli <kat.li@vaynermedia.com>
2019-04-19 16:20:06 -0400

    first pass

--- ticket.py
+++ ticket.py
@@ -1,6 +1,17 @@
 """
+# CREATE TICKET FROM CODE
+from shotgun import ticket
+reload(ticket)
+ticket.create()
+
+# CREATE TICKET FROM UI
+from shotgun import ticket
+reload(ticket)
+ticket.get_window()
 """
+from . import *
 from PySide2 import QtCore, QtWidgets, QtUiTools
+from pymel.core import workspace, sceneName
 
 
 def get_window():
@@ -12,6 +23,25 @@ def get_window():
 
     mw = MyWindow()
     mw.ui.show()
+
+
+def create(title="", type=None, description="", script_editor="", maya_file=""):
+    if not maya_file:
+        maya_file = sceneName()
+
+    data = {
+        "title": title,
+        "project": project,  # from __init__
+        "sg_ticket_type": type,
+        "description": description,
+        "sg_script_editor_dialogue": script_editor,
+        "sg_maya_file": maya_file
+    }
+    ticket = sg.create("Ticket", data)
+    print ">> Created ticket #{0}\nhttps://vaynerproductions.shotgunstudio.com/page/10070#Ticket_{0}".format(
+        ticket["id"]
+    ),
+    return ticket
 
 
 class MyWindow(QtWidgets.QDialog):
@@ -28,5 +58,17 @@ class MyWindow(QtWidgets.QDialog):
         ui_file.close()
         return ui
 
+    def create_ticket(self):
+        title = self.ui.title_lne.text()
+        type = self.ui.type_cbx.currentText()
+        description = self.ui.description_txt.toPlainText()
+        script_editor = self.ui.script_txt.toPlainText()
+
+        self.ui.close()
+        create(title=title, type=type, description=description, script_editor=script_editor)
+        return
+
     def init_ui(self):
+        self.ui.attach_btn.deleteLater()
+        self.ui.submit_btn.clicked.connect(self.create_ticket)
         return
