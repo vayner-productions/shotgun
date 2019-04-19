d820a6baa50ed773e2a8fa6d72a04c286fb7a605
vp-kli <kat.li@vaynermedia.com>
2019-04-19 14:39:20 -0400

    template ui

---
+++ ticket.py
@@ -0,0 +1,32 @@
+"""
+"""
+from PySide2 import QtCore, QtWidgets, QtUiTools
+
+
+def get_window():
+    global mw
+    try:
+        mw.ui.close()
+    except:
+        pass
+
+    mw = MyWindow()
+    mw.ui.show()
+
+
+class MyWindow(QtWidgets.QDialog):
+    def __init__(self):
+        self.ui = self.import_ui()
+        self.init_ui()
+
+    def import_ui(self):
+        ui_path = __file__.split(".")[0] + ".ui"
+        loader = QtUiTools.QUiLoader()
+        ui_file = QtCore.QFile(ui_path)
+        ui_file.open(QtCore.QFile.ReadOnly)
+        ui = loader.load(ui_file)
+        ui_file.close()
+        return ui
+
+    def init_ui(self):
+        return
