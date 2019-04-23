25ff1fc086de995b08755f06c8497da16f0fa258
vp-kli <kat.li@vaynermedia.com>
2019-04-23 16:43:26 -0400

    MyWindow()

--- checkout_scene.py
+++ checkout_scene.py
@@ -1,7 +1,7 @@
 """
 """
 from . import *
-from pymel.core import workspace, openFile, saveAs, warning
+from pymel.core import workspace, openFile, saveAs, displayInfo
 from pymel.util import path
 from PySide2 import QtCore, QtWidgets, QtUiTools
 import maya.mel as mel
@@ -20,125 +20,100 @@ def get_window():
 
 class Checkout(object):
     def __init__(self):
-        # self.processed_file()
-        # self.published_file()
+        self.checkout_file = None
         return
 
-    def processed_file(self):
-        processed_path = path(workspace.expandName(workspace.fileRules["scene"]))
-        search_pattern = "{}_processed.*.ma".format(processed_path.basename())
+    def processed_file(self, open_file=1):
+        scene_path = workspace.path.joinpath(workspace.fileRules["scene"])
+        search_pattern = str(scene_path.namebase + "_processed.*.ma")
         if search_pattern[:3].isdigit():
             search_pattern = search_pattern[4:]
-        processed_files = sorted(processed_path.files(search_pattern))[::-1]
 
-        processed_file = None
+        processed_files = sorted(scene_path.files(search_pattern))[::-1]
         if processed_files:
-            processed_file = sorted(processed_path.files(search_pattern))[::-1][0].normpath()
-        return processed_file
+            self.checkout_file = processed_files[0].normpath()
+        else:
+            checkout_filename = path(workspace.fileRules["scene"]).namebase + "_processed.0001.ma"
+            if checkout_filename[:3].isdigit():
+                checkout_filename = checkout_filename[4:]
 
-    def published_file(self):
-        scene_directory = workspace.fileRules["scene"]
+            self.checkout_file = workspace.path.joinpath(
+                workspace.fileRules["scene"],
+                checkout_filename
+            ).normpath()
 
-        key = path(scene_directory).dirname().basename()[3:]
-        data = {
-            "Rigs": "sg_file",
-            "Assets": "sg_file",
-            "Cameras": "_Cam",
-            "Layouts": "sg_maya_layout",
-            "Animation": "_Anim",
-            "Lighting": "sg_maya_light"
-        }
-        entity_type = "Shot"
-        if key in ["Rigs", "Assets"]:
-            entity_type = "Asset"
-        entity_code = path(scene_directory).basename()
+        if open_file and self.checkout_file.exists():
+            openFile(self.checkout_file, f=1)
+        elif open_file and not self.checkout_file.exists():
+            saveAs(self.checkout_file, f=1)
+        return self.checkout_file
+
+    def increment_file(self, open_file=1):
+        self.processed_file()
+
+        if self.checkout_file.exists():
+            self.checkout_file = self.checkout_file.split(".")
+            self.checkout_file[1] = str(int(self.checkout_file[1]) + 1).zfill(4)
+            self.checkout_file = ".".join(self.checkout_file)
+
+        if open_file:
+            saveAs(self.checkout_file, f=1)
+        return self.checkout_file
+
+    def published_file(self, open_file=1):
+        scene_process, entity_name = workspace.fileRules["scene"].split("/")[1:]
 
         published_file = None
-        if key == "Cameras" or key == "Animation":
-            version_additional_filter_presets = [
-                {
-                    "preset_name": "LATEST",
-                    "latest_by": "ENTITIES_CREATED_AT"
-                }
-            ]
-            published_file = sg.find_one(
-                "Version",
-                [["project", "is", project], ["code", "contains", entity_code+data[key]]],
-                fields=["sg_maya_file"],
-                additional_filter_presets=version_additional_filter_presets
-            )["sg_maya_file"]["local_path_windows"]
-        else:
-            entity_filters = [
-                ["project", "is", project],
-                ["code", "is", entity_code]
-            ]
+        if "Assets" in scene_process or "Rigs" in scene_process:
+            try:
+                published_file = path(sg.find_one(
+                    "Asset",
+                    [["project", "is", project], ["code", "is", entity_name]],
+                    ["sg_file"]
+                )["sg_file"]["local_path"])
+            except:
+                pass
+        elif "Cameras" in scene_process:
+            try:
+                published_file = path(sg.find_one(
+                    "Version",
+                    [["project", "is", project], ["code", "contains", entity_name + "_CAM"]],
+                    ["sg_maya_file"],
+                    additional_filter_presets=[{"preset_name": "LATEST", "latest_by": "ENTITIES_CREATED_AT"}]
+                )["sg_maya_file"]["local_path"])
+            except:
+                pass
+        elif "Animation" in scene_process:
+            try:
+                published_file = path(sg.find_one(
+                    "Version",
+                    [["project", "is", project], ["code", "contains", entity_name + "_ANIM"]],
+                    ["sg_maya_file"],
+                    additional_filter_presets=[{"preset_name": "LATEST", "latest_by": "ENTITIES_CREATED_AT"}]
+                )["sg_maya_file"]["local_path"])
+            except:
+                pass
+        elif "Lighting" in scene_process:
+            try:
+                published_file = path(sg.find_one(
+                    "Shot",
+                    [["project", "is", project], ["code", "is", entity_name]],
+                    ["sg_maya_light"]
+                )["sg_maya_light"]["local_path"])
+            except:
+                pass
 
-            if key == "Rigs":
-                entity_filters += [["sg_asset_type", "is", "CG Rig"]]
-            elif key == "Assets":
-                entity_filters += [["sg_asset_type", "is", "CG Model"]]
-
-            published_file = sg.find_one(
-                entity_type,
-                entity_filters,
-                [data[key]]
-            )[data[key]]["local_path_windows"]
-        published_file = path(published_file).normpath()
-        published_file = "{}".format(published_file)
-        return published_file
-
-    def run(self, checkout_type="processed", checkout_file=None):
-        """
-        saves any file into working directory and renames it to Vayner's naming convention
-
-        "processed" - opens the latest working file
-        "published" - opens the latest published file, saves and increments as processed file
-        "other" - opens any file, saves and increments as processed file
-        "increment" - versions up the active file to match the naming convention for the set project. This is useful for
-        copy lighting setup from one shot to another. To do so,
-        1. open file with lighting setup
-        2. set project to the shot to copy to
-        3. run(checkout_type="increment")
-        * may need to remove referenced assets of old shot
-        4. build scene to update assets for the new shot
-
-        ** set project to use the correct working directory **
-        ** build scene references/updates assets specific to active shot **
-        :param checkout_type: [str] "processed", "published", "other"
-        :param checkout_file:
-        :return: [str] working file path
-        """
-        if checkout_type == "processed":
-            checkout_file = self.processed_file()
-            openFile(self.processed_file(), f=1)
-        elif checkout_type == "published":
-            checkout_file = self.processed_file().split(".")
-            checkout_file[1] = str(int(checkout_file[1]) + 1).zfill(4)
-            checkout_file = ".".join(checkout_file)
-            openFile(self.published_file(), f=1)
-            saveAs(checkout_file)
-        elif checkout_type == "increment":
-            checkout_file = self.processed_file()
-            if not checkout_file:
-                processed_path = path(workspace.expandName(workspace.fileRules["scene"]))
-                search_pattern = "{}_processed.*.ma".format(processed_path.basename())
-                checkout_file = path(processed_path + "/" + search_pattern.replace("*", "0001")).normpath()
-                checkout_file = r"{}".format(checkout_file)  # converts from path object to string
-            else:
-                checkout_file = checkout_file.split(".")
-                checkout_file[1] = str(int(checkout_file[1]) + 1).zfill(4)
-                checkout_file = ".".join(checkout_file)
-            saveAs(checkout_file)
-        elif checkout_type == "other" and checkout_file:
-            openFile(checkout_file, f=1)
-            checkout_file = self.processed_file().split(".")
-            checkout_file[1] = str(int(checkout_file[1]) + 1).zfill(4)
-            checkout_file = ".".join(checkout_file)
-            saveAs(checkout_file)
-        else:
-            return warning("Wrong checkout type and/or missing checkout file.")
-        print ">> Checked out file: ..{}\n".format(checkout_file.split("scenes")[1]),
-        return checkout_file
+        if published_file and open_file:
+            self.increment_file(open_file=0)
+            openFile(published_file, f=1)
+            saveAs(self.checkout_file, f=1)
+            return self.checkout_file
+        elif not published_file and open_file:
+            self.processed_file()
+            displayInfo("No published file to checkout, opened latest working file: ../{}".format(
+                self.checkout_file.basename()
+            ))
+            return published_file
 
 
 class SetProject(object):
@@ -194,7 +169,7 @@ class SetProject(object):
         workspace.fileRules["shaders"] = "data/001_Shaders"
 
         mel.eval('setProject \"' + self.project_path + '\"')
-        print ">> project set\n",
+        # print ">> Project set: {}\n".format(self.project_path),
         return
 
 
@@ -203,6 +178,7 @@ class MyWindow(SetProject, Checkout, QtWidgets.QDialog):
         super(MyWindow, self).__init__(**kwargs)
         self.ui = self.import_ui()
         self.init_ui()
+        return
 
     def import_ui(self):
         ui_path = __file__.split(".")[0] + ".ui"
@@ -214,13 +190,14 @@ class MyWindow(SetProject, Checkout, QtWidgets.QDialog):
         return ui
 
     def change_entity_items(self):
-        scene_process = self.ui.scene_cbx.currentText()
+        scene_process = self.ui.process_cbx.currentText()
         scene_process_folder = self.scene_dict[scene_process]
 
         scene_process_dir = path(r"{}{}{}".format(
             self.project_path,
             "/scenes/",
-            scene_process_folder))
+            scene_process_folder
+        ))
 
         folders = []
         self.ui.asset_cbx.clear()
@@ -234,15 +211,27 @@ class MyWindow(SetProject, Checkout, QtWidgets.QDialog):
         return
 
     def checkout(self):
-
+        scene = "/".join([
+            "scenes",
+            self.scene_dict[self.ui.process_cbx.currentText()],
+            self.ui.asset_cbx.currentText()
+        ])
+        alembic = "scenes/06_Cache/08_Animation/{}".format(self.ui.asset_cbx.currentText())
         self.set_project(scene=scene, alembic=alembic)
+
+        if self.ui.checkout_cbx.currentText() == "Published File":
+            self.published_file()
+        elif self.ui.checkout_cbx.currentText() == "Working File":
+            self.processed_file()
+
+        self.ui.close()
         return
 
     def init_ui(self):
-        self.ui.scene_cbx.currentTextChanged.connect(self.change_entity_items)
+        self.ui.process_cbx.currentTextChanged.connect(self.change_entity_items)
 
         items = [item[3:] for item in sorted(self.scene_dict.values())]
-        self.ui.scene_cbx.addItems(items)
+        self.ui.process_cbx.addItems(items)
 
-        self.ui.checkout_cbx.clicked.connect(self.checkout)
+        self.ui.checkout_btn.clicked.connect(self.checkout)
         return
